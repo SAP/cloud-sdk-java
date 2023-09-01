3a6b559415f85400311c590873dd234747de204d
Kevin Hudemann <kevin.hudemann@sap.com>
2023-09-01 09:30:19 +0200

    Apply suggestions from code review

--- .pipeline/scripts/print-coverage.py
+++ .pipeline/scripts/print-coverage.py
@@ -19,42 +19,52 @@ def get_jacoco_thresholds():
                 config['stages']['test']['coverageThresholds']['method'],
                 config['stages']['test']['coverageThresholds']['class'])
 
+def get_threshold_for_summary(threshold):
+    return threshold if threshold >= 0 else 'unlimited'
+
 def write_jacoco_github_output(jacoco_report_pattern):
     jacoco_report_files = glob(jacoco_report_pattern, recursive=True)
 
-    instruction_coverage, branch_coverage, line_coverage, complexity_coverage, method_coverage, class_coverage = compute_coverage(jacoco_report_files)
+    instruction_coverage, branch_coverage, line_coverage, complexity_coverage, method_coverage, class_coverage, classes_covered, number_of_classes = compute_coverage(jacoco_report_files)
     instruction_threshold, branch_threshold, line_threshold, complexity_threshold, method_threshold, class_threshold = get_jacoco_thresholds()
+
+    summary_instruction_threshold = get_threshold_for_summary(instruction_threshold)
+    summary_branch_threshold = get_threshold_for_summary(branch_threshold)
+    summary_line_threshold = get_threshold_for_summary(line_threshold)
+    summary_complexity_threshold = get_threshold_for_summary(complexity_threshold)
+    summary_method_threshold = get_threshold_for_summary(method_threshold)
+    summary_class_threshold = get_threshold_for_summary(class_threshold)
 
     if 'GITHUB_STEP_SUMMARY' in os.environ:
         with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as f:
             print('## JaCoCo Result', file=f)
             print('| Category | Actual Coverage | Coverage Threshold |', file=f)
             print('| --------------- | --------------- | ------------------ |', file=f)
-            print(f"| Instruction Coverage | {instruction_coverage} | {instruction_threshold} |", file=f)
-            print(f"| Branch Coverage | {branch_coverage} | {branch_threshold} |", file=f)
-            print(f"| Line Coverage | {line_coverage} | {line_threshold} |", file=f)
-            print(f"| Complexity Coverage | {complexity_coverage} | {complexity_threshold} |", file=f)
-            print(f"| Method Coverage | {method_coverage} | {method_threshold} |", file=f)
-            print(f"| Class Coverage | {class_coverage} | {class_threshold} |", file=f)
+            print(f"| Class Coverage | {class_coverage}% ({classes_covered}/{number_of_classes} covered) | {summary_class_threshold}% |", file=f)
+            print(f"| Method Coverage | {method_coverage}% | {summary_method_threshold}% |", file=f)
+            print(f"| Line Coverage | {line_coverage}% | {summary_line_threshold}% |", file=f)
+            print(f"| Complexity Coverage | {complexity_coverage}% | {summary_complexity_threshold}% |", file=f)
+            print(f"| Branch Coverage | {branch_coverage}% | {summary_branch_threshold}% |", file=f)
+            print(f"| Instruction Coverage | {instruction_coverage}% | {summary_instruction_threshold}% |", file=f)
 
-    print(f"Instruction Coverage: {instruction_coverage}, Instruction Threshold: {instruction_threshold}")
-    print(f"Branch Coverage: {branch_coverage}, Branch Threshold: {branch_threshold}")
-    print(f"Line Coverage: {line_coverage}, Line Threshold: {line_threshold}")
-    print(f"Complexity Coverage: {complexity_coverage}, Complexity Threshold: {complexity_threshold}")
-    print(f"Method Coverage: {method_coverage}, Method Threshold: {method_threshold}")
-    print(f"Class Coverage: {class_coverage}, Class Threshold: {class_threshold}")
+    print(f"Class Coverage: {class_coverage}% ({classes_covered}/{number_of_classes}), Class Threshold: {summary_class_threshold}%")
+    print(f"Method Coverage: {method_coverage}%, Method Threshold: {summary_method_threshold}%")
+    print(f"Line Coverage: {line_coverage}%, Line Threshold: {summary_line_threshold}%")
+    print(f"Complexity Coverage: {complexity_coverage}%, Complexity Threshold: {summary_complexity_threshold}%")
+    print(f"Branch Coverage: {branch_coverage}%, Branch Threshold: {summary_branch_threshold}%")
+    print(f"Instruction Coverage: {instruction_coverage}%, Instruction Threshold: {summary_instruction_threshold}%")
 
-    if instruction_coverage < instruction_threshold:
+    if instruction_threshold >= 0 and instruction_coverage < instruction_threshold:
         sys.exit('Instruction coverage below threshold')
-    elif branch_coverage < branch_threshold:
+    elif branch_threshold >= 0 and branch_coverage < branch_threshold:
         sys.exit('Branch coverage below threshold')
-    elif line_coverage < line_threshold:
+    elif line_threshold >= 0 and line_coverage < line_threshold:
         sys.exit('Line coverage below threshold')
-    elif complexity_coverage < complexity_threshold:
+    elif complexity_threshold >= 0 and complexity_coverage < complexity_threshold:
         sys.exit('Complexity coverage below threshold')
-    elif method_coverage < method_threshold:
+    elif method_threshold >= 0 and method_coverage < method_threshold:
         sys.exit('Method coverage below threshold')
-    elif class_coverage < class_threshold:
+    elif class_threshold >= 0 and class_coverage < class_threshold:
         sys.exit('Class coverage below threshold')
 
 def compute_coverage(file_list) :
@@ -95,7 +105,9 @@ def compute_coverage(file_list) :
             calculate_percentage(covered_lines, missed_lines),
             calculate_percentage(complexity_covered, complexity_missed),
             calculate_percentage(method_covered, method_missed),
-            calculate_percentage(classes_covered, classes_missed))
+            calculate_percentage(classes_covered, classes_missed),
+            classes_covered,
+            classes_covered + classes_missed)
 
 def calculate_percentage(covered, missed) :
     if covered == 0 & missed == 0 :
