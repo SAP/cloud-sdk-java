e702248b3f149f60785ab5c40c3dc0b80d97313d
Rachael Newbigging <rachael.newbigging@sap.com>
2023-10-23 10:14:37 +0200

    formatting

--- cloudplatform/tenant-scp-cf/src/test/java/com/sap/cloud/sdk/cloudplatform/tenant/ScpCfTenantFacadeXsuaaTest.java
+++ cloudplatform/tenant-scp-cf/src/test/java/com/sap/cloud/sdk/cloudplatform/tenant/ScpCfTenantFacadeXsuaaTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.sap.cloud.sdk.cloudplatform.exception.CloudPlatformException;
 import com.sap.cloud.sdk.cloudplatform.security.AuthToken;
 import com.sap.cloud.sdk.cloudplatform.security.AuthTokenAccessor;
 import com.sap.cloud.sdk.cloudplatform.security.ScpCfAuthTokenFacade;
