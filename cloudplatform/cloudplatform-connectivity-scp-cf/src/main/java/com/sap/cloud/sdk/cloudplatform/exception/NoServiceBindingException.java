3783c43cf82f533fa8a10fa40ab6a7a8ea876881
Rachael Newbigging <rachael.newbigging@sap.com>
2023-11-06 11:37:47 +0100

    namespace fix

--- cloudplatform/cloudplatform-connectivity-scp-cf/src/main/java/com/sap/cloud/sdk/cloudplatform/exception/NoServiceBindingException.java
+++ cloudplatform/cloudplatform-connectivity-scp-cf/src/main/java/com/sap/cloud/sdk/cloudplatform/exception/NoServiceBindingException.java
@@ -2,11 +2,9 @@
  * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
  */
 
-package com.sap.cloud.sdk.cloudplatform.connectivity.exception;
+package com.sap.cloud.sdk.cloudplatform.exception;
 
 import javax.annotation.Nullable;
-
-import com.sap.cloud.sdk.cloudplatform.exception.CloudPlatformException;
 
 import lombok.NoArgsConstructor;
 
