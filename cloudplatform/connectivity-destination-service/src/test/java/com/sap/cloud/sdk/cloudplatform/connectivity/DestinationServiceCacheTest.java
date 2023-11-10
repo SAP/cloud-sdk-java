058bd10d982e86dc642f98fae983bb00a05fd2ef
Alexander Dümont <alexander.duemont@sap.com>
2023-11-10 15:46:55 +0100

    Fix merge

--- cloudplatform/connectivity-destination-service/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/DestinationServiceCacheTest.java
+++ cloudplatform/connectivity-destination-service/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/DestinationServiceCacheTest.java
@@ -23,84 +23,84 @@ public class DestinationServiceCacheTest
     @After
     public void resetCache()
     {
-        ScpCfDestinationLoader.Cache.reset();
+        DestinationService.Cache.reset();
     }
 
     @Test
     public void testDisable()
     {
-        assertThat(ScpCfDestinationLoader.Cache.isEnabled()).isTrue();
+        assertThat(DestinationService.Cache.isEnabled()).isTrue();
 
         // sut
-        ScpCfDestinationLoader.Cache.disable();
-        assertThat(ScpCfDestinationLoader.Cache.isEnabled()).isFalse();
-        assertThatCode(ScpCfDestinationLoader.Cache::enableChangeDetection).isInstanceOf(IllegalStateException.class);
-        assertThatCode(ScpCfDestinationLoader.Cache::instanceSingle).isInstanceOf(IllegalStateException.class);
-        assertThatCode(ScpCfDestinationLoader.Cache::instanceAll).isInstanceOf(IllegalStateException.class);
+        DestinationService.Cache.disable();
+        assertThat(DestinationService.Cache.isEnabled()).isFalse();
+        assertThatCode(DestinationService.Cache::enableChangeDetection).isInstanceOf(IllegalStateException.class);
+        assertThatCode(DestinationService.Cache::instanceSingle).isInstanceOf(IllegalStateException.class);
+        assertThatCode(DestinationService.Cache::instanceAll).isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     public void testReset()
     {
-        final Cache<CacheKey, ?> cacheSingle = ScpCfDestinationLoader.Cache.instanceSingle();
-        final Cache<CacheKey, ?> cacheAll = ScpCfDestinationLoader.Cache.instanceAll();
+        final Cache<CacheKey, ?> cacheSingle = DestinationService.Cache.instanceSingle();
+        final Cache<CacheKey, ?> cacheAll = DestinationService.Cache.instanceAll();
 
         // sut
-        ScpCfDestinationLoader.Cache.reset();
-        assertThat(cacheSingle).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
-        assertThat(cacheAll).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
+        DestinationService.Cache.reset();
+        assertThat(cacheSingle).isNotSameAs(DestinationService.Cache.instanceSingle());
+        assertThat(cacheAll).isNotSameAs(DestinationService.Cache.instanceAll());
     }
 
     @Test
     public void testChangeDetection()
     {
-        final Cache<CacheKey, ?> cacheSingle = ScpCfDestinationLoader.Cache.instanceSingle();
-        final Cache<CacheKey, ?> cacheAll = ScpCfDestinationLoader.Cache.instanceAll();
-        assertThat(ScpCfDestinationLoader.Cache.isChangeDetectionEnabled()).isFalse();
+        final Cache<CacheKey, ?> cacheSingle = DestinationService.Cache.instanceSingle();
+        final Cache<CacheKey, ?> cacheAll = DestinationService.Cache.instanceAll();
+        assertThat(DestinationService.Cache.isChangeDetectionEnabled()).isFalse();
 
         // sut
-        ScpCfDestinationLoader.Cache.enableChangeDetection();
-        assertThat(ScpCfDestinationLoader.Cache.isChangeDetectionEnabled()).isTrue();
-        assertThat(cacheSingle).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
-        assertThat(cacheAll).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
+        DestinationService.Cache.enableChangeDetection();
+        assertThat(DestinationService.Cache.isChangeDetectionEnabled()).isTrue();
+        assertThat(cacheSingle).isNotSameAs(DestinationService.Cache.instanceSingle());
+        assertThat(cacheAll).isNotSameAs(DestinationService.Cache.instanceAll());
     }
 
     @Test
     public void testExpiration()
     {
-        final Option<Duration> oldDuration = ScpCfDestinationLoader.Cache.getExpirationDuration();
+        final Option<Duration> oldDuration = DestinationService.Cache.getExpirationDuration();
         final Duration newDuration = Duration.ofSeconds(1);
-        Cache<CacheKey, ?> cacheSingle = ScpCfDestinationLoader.Cache.instanceSingle();
-        Cache<CacheKey, ?> cacheAll = ScpCfDestinationLoader.Cache.instanceAll();
+        Cache<CacheKey, ?> cacheSingle = DestinationService.Cache.instanceSingle();
+        Cache<CacheKey, ?> cacheAll = DestinationService.Cache.instanceAll();
 
         // sut
-        ScpCfDestinationLoader.Cache.setExpiration(newDuration, CacheExpirationStrategy.WHEN_CREATED);
-        assertThat(ScpCfDestinationLoader.Cache.getExpirationDuration()).isNotEqualTo(oldDuration);
-        assertThat(ScpCfDestinationLoader.Cache.getExpirationDuration()).containsExactly(newDuration);
-        assertThat(cacheSingle).isNotSameAs(cacheSingle = ScpCfDestinationLoader.Cache.instanceSingle());
-        assertThat(cacheAll).isNotSameAs(cacheAll = ScpCfDestinationLoader.Cache.instanceSingle());
+        DestinationService.Cache.setExpiration(newDuration, CacheExpirationStrategy.WHEN_CREATED);
+        assertThat(DestinationService.Cache.getExpirationDuration()).isNotEqualTo(oldDuration);
+        assertThat(DestinationService.Cache.getExpirationDuration()).containsExactly(newDuration);
+        assertThat(cacheSingle).isNotSameAs(cacheSingle = DestinationService.Cache.instanceSingle());
+        assertThat(cacheAll).isNotSameAs(cacheAll = DestinationService.Cache.instanceAll());
 
         // sut
-        ScpCfDestinationLoader.Cache.disableExpiration();
-        assertThat(ScpCfDestinationLoader.Cache.getExpirationDuration()).isEmpty();
-        assertThat(cacheSingle).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
-        assertThat(cacheAll).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
+        DestinationService.Cache.disableExpiration();
+        assertThat(DestinationService.Cache.getExpirationDuration()).isEmpty();
+        assertThat(cacheSingle).isNotSameAs(DestinationService.Cache.instanceSingle());
+        assertThat(cacheAll).isNotSameAs(DestinationService.Cache.instanceAll());
     }
 
     @Test
     public void testSize()
     {
-        Cache<CacheKey, ?> cacheSingle = ScpCfDestinationLoader.Cache.instanceSingle();
-        Cache<CacheKey, ?> cacheAll = ScpCfDestinationLoader.Cache.instanceAll();
+        Cache<CacheKey, ?> cacheSingle = DestinationService.Cache.instanceSingle();
+        Cache<CacheKey, ?> cacheAll = DestinationService.Cache.instanceAll();
 
         // sut
-        ScpCfDestinationLoader.Cache.setSizeLimit(100);
-        assertThat(cacheSingle).isNotSameAs(cacheSingle = ScpCfDestinationLoader.Cache.instanceSingle());
-        assertThat(cacheAll).isNotSameAs(cacheAll = ScpCfDestinationLoader.Cache.instanceSingle());
+        DestinationService.Cache.setSizeLimit(100);
+        assertThat(cacheSingle).isNotSameAs(cacheSingle = DestinationService.Cache.instanceSingle());
+        assertThat(cacheAll).isNotSameAs(cacheAll = DestinationService.Cache.instanceAll());
 
         // sut
-        ScpCfDestinationLoader.Cache.disableSizeLimit();
-        assertThat(cacheSingle).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
-        assertThat(cacheAll).isNotSameAs(ScpCfDestinationLoader.Cache.instanceSingle());
+        DestinationService.Cache.disableSizeLimit();
+        assertThat(cacheSingle).isNotSameAs(DestinationService.Cache.instanceSingle());
+        assertThat(cacheAll).isNotSameAs(DestinationService.Cache.instanceAll());
     }
 }
