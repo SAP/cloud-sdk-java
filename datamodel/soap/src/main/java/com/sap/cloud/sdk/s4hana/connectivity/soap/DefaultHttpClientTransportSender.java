e9300b96f448c39d63678aff3ed76727137a199d
Kavitha <kavitha.sivakumar@sap.com>
2023-11-21 14:00:21 +0100

    Bump Axis2 to 1.8.2 and it's associated changes

--- datamodel/soap/src/main/java/com/sap/cloud/sdk/s4hana/connectivity/soap/DefaultHttpClientTransportSender.java
+++ datamodel/soap/src/main/java/com/sap/cloud/sdk/s4hana/connectivity/soap/DefaultHttpClientTransportSender.java
@@ -4,14 +4,15 @@
 
 package com.sap.cloud.sdk.s4hana.connectivity.soap;
 
-import org.apache.axis2.transport.http.AbstractHTTPSender;
+import org.apache.axis2.transport.http.HTTPSender;
 import org.apache.axis2.transport.http.impl.httpclient4.HTTPClient4TransportSender;
+import org.apache.axis2.transport.http.impl.httpclient4.HTTPSenderImpl;
 
 class DefaultHttpClientTransportSender extends HTTPClient4TransportSender
 {
     @Override
-    protected AbstractHTTPSender createHTTPSender()
+    protected HTTPSender createHTTPSender()
     {
-        return new DefaultHttpSender();
+        return new HTTPSenderImpl();
     }
 }
