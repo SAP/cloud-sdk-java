e6286700608c01d35b6808d0711c577c44f5e7c1
Rachael Newbigging <rachael.newbigging@sap.com>
2024-01-03 09:50:40 +0100

    copyright updated

--- datamodel/odata/odata-generator/src/test/resources/oDataGeneratorIntegrationTest/minimalTestWithExplicitDeprecation/output/testcomparison/namespaces/minimalmetadata/selectable/SimplePersonSelectable.java
+++ datamodel/odata/odata-generator/src/test/resources/oDataGeneratorIntegrationTest/minimalTestWithExplicitDeprecation/output/testcomparison/namespaces/minimalmetadata/selectable/SimplePersonSelectable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
+ * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
  */
 
 package testcomparison.namespaces.minimalmetadata.selectable;
