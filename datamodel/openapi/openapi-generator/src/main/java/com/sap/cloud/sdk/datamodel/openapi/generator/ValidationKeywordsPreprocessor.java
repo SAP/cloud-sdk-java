d7ea818b6337d6378ba2d2f4db7eeeaff9bb71fc
Kavitha <kavitha.sivakumar@sap.com>
2024-01-26 17:58:10 +0530

    feat: fix Initial solution idea

--- datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/ValidationKeywordsPreprocessor.java
+++ datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/ValidationKeywordsPreprocessor.java
@@ -6,29 +6,34 @@ package com.sap.cloud.sdk.datamodel.openapi.generator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import com.sap.cloud.sdk.datamodel.openapi.generator.exception.OpenApiGeneratorException;
 
 class ValidationKeywordsPreprocessor implements PreprocessingStep
 {
     private final List<String> POTENTIALLY_UNSUPPORTED_VALIDATION_KEYWORDS = Arrays.asList("anyOf", "oneOf");
-    private final String PATHS_NODE = "paths";
-    private final String COMPONENTS_NODE = "components";
-    private final String SCHEMAS_NODE = "schemas";
+    private static final String ANY_OF = "anyOf";
+    private static final String ONE_OF = "oneOf";
+    private static final String PATHS_NODE = "paths";
+    private static final String COMPONENTS_NODE = "components";
+    private static final String SCHEMAS_NODE = "schemas";
 
     @Nonnull
     @Override
-    public PreprocessingStepResult execute( @Nonnull final JsonNode input, @Nonnull final ObjectMapper objectMapper )
+    public PreprocessingStepResult execute(@Nonnull final JsonNode input, @Nonnull final ObjectMapper objectMapper)
     {
         final JsonNode pathsNode = input.path(PATHS_NODE);
-        checkForValidatorsInPaths(pathsNode);
+        final boolean isPathUpdated = checkAndReplaceValidatorsInPaths(input);
 
         final JsonNode schemasNode = input.path(COMPONENTS_NODE).path(SCHEMAS_NODE);
-        checkForValidatorsInSchemas(schemasNode);
+        final boolean isSchemaUpdated = checkForValidatorsInSchemas(schemasNode);
 
         return new PreprocessingStepResult()
         {
@@ -43,27 +48,31 @@ class ValidationKeywordsPreprocessor implements PreprocessingStep
             @Override
             public boolean changesApplied()
             {
-                return false;
+                return (isPathUpdated || isSchemaUpdated);
             }
         };
     }
 
-    private void checkForValidatorsInPaths( final JsonNode pathsNode )
+    private boolean checkAndReplaceValidatorsInPaths(final JsonNode pathsNode)
     {
-        for( final String field : POTENTIALLY_UNSUPPORTED_VALIDATION_KEYWORDS ) {
-            final boolean isUnsupported = pathsNode.findValue(field) != null;
-            if( isUnsupported ) {
-                throw new OpenApiGeneratorException(
-                    "The OpenAPI spec contains keyword "
-                        + field
-                        + " inside the Paths which is not currently supported during generation. Please remove these fields for the generation to succeed");
-            }
-        }
+        final AtomicBoolean isInputUpdated = new AtomicBoolean(false);
+        pathsNode.findParents("schema").forEach(node -> {
+                ObjectNode schemaNode = (ObjectNode) node.get("schema");
+                if (schemaNode.has(ANY_OF) || schemaNode.has(ONE_OF)) {
+                    schemaNode.removeAll();
+
+                    schemaNode.put("type", "object");
+                    schemaNode.put("additionalProperties", true);
+                    isInputUpdated.set(true);
+                }
+        });
+        return isInputUpdated.get();
     }
 
-    private void checkForValidatorsInSchemas( final JsonNode schemasNode )
+    private boolean checkForValidatorsInSchemas( final JsonNode schemasNode )
     {
-        for( final String field : POTENTIALLY_UNSUPPORTED_VALIDATION_KEYWORDS ) {
+        return false;
+  /*      for( final String field : POTENTIALLY_UNSUPPORTED_VALIDATION_KEYWORDS ) {
             for( final JsonNode schema : schemasNode ) {
                 for( final JsonNode schemaChild : schema ) {
                     final boolean isUnsupported = schemaChild.findValue(field) != null;
@@ -79,5 +88,5 @@ class ValidationKeywordsPreprocessor implements PreprocessingStep
                 }
             }
         }
-    }
+  */  }
 }
