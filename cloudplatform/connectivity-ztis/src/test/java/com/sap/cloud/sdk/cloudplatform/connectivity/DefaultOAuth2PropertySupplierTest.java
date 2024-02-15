a4853abe5140c9d35c491c866a3b4336877e6127
Matthias Kuhr <matthias.kuhr@sap.com>
2024-02-15 15:03:35 +0100

    stash

---
+++ cloudplatform/connectivity-ztis/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/DefaultOAuth2PropertySupplierTest.java
@@ -0,0 +1,195 @@
+package com.sap.cloud.sdk.cloudplatform.connectivity;
+
+import static com.sap.cloud.sdk.cloudplatform.connectivity.DefaultOAuth2PropertySupplier.convert;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import org.junit.jupiter.api.Test;
+
+import com.sap.cloud.environment.servicebinding.api.DefaultServiceBinding;
+import com.sap.cloud.environment.servicebinding.api.ServiceBinding;
+import com.sap.cloud.environment.servicebinding.api.ServiceIdentifier;
+import com.sap.cloud.sdk.cloudplatform.connectivity.exception.DestinationAccessException;
+import com.sap.cloud.security.config.ClientCertificate;
+import com.sap.cloud.security.config.CredentialType;
+
+import lombok.RequiredArgsConstructor;
+
+class DefaultOAuth2PropertySupplierTest
+{
+    private DefaultOAuth2PropertySupplier sut;
+
+    @Test
+    void testValueConverter()
+    {
+        assertThat(convert(null, String.class)).isNull();
+        assertThat(convert(null, URI.class)).isNull();
+        assertThat(convert(null, Integer.class)).isNull();
+        assertThat(convert(null, CredentialType.class)).isNull();
+
+        assertThat(convert("foo", String.class)).isEqualTo("foo");
+        assertThat(convert(1337, String.class)).isEqualTo("1337");
+
+        assertThat(convert(1337, Integer.class)).isEqualTo(1337);
+        assertThat(convert("1337", Integer.class)).isEqualTo(1337);
+
+        assertThat(convert("https://foo.bar", URI.class)).isEqualTo(URI.create("https://foo.bar"));
+        assertThat(convert(URI.create("https://foo.bar"), URI.class)).isEqualTo(URI.create("https://foo.bar"));
+        assertThatThrownBy(() -> convert("not a valid uri", URI.class))
+            .isExactlyInstanceOf(DestinationAccessException.class)
+            .hasCauseExactlyInstanceOf(URISyntaxException.class);
+
+        assertThat(convert(CredentialType.X509, CredentialType.class)).isEqualTo(CredentialType.X509);
+        assertThat(convert(CredentialType.X509.toString(), CredentialType.class)).isEqualTo(CredentialType.X509);
+        assertThat(convert("X509_GENERATED", CredentialType.class)).isEqualTo(CredentialType.X509);
+        assertThatThrownBy(() -> convert("not a valid credential type", CredentialType.class))
+            .isExactlyInstanceOf(DestinationAccessException.class)
+            .hasCauseExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testCredentialAccess()
+    {
+        final ServiceBinding binding =
+            new ServiceBindingBuilder(ServiceIdentifier.DESTINATION)
+                .with("name", "asdf")
+                .with("credentials.name", "foo")
+                .with("credentials.uaa.name", "bar")
+                .build();
+
+        final ServiceBindingDestinationOptions options = ServiceBindingDestinationOptions.forService(binding).build();
+
+        sut = new DefaultOAuth2PropertySupplier(options, Collections.emptyList());
+
+        assertThat(sut.getCredential(String.class, "name")).contains("foo");
+        assertThat(sut.getCredential(String.class, "uaa", "name")).contains("bar");
+        assertThat(sut.getCredential(String.class, "bar")).isEmpty();
+
+        assertThatThrownBy(() -> sut.getCredentialOrThrow(String.class, "bar"))
+            .isInstanceOf(DestinationAccessException.class);
+    }
+
+    @Test
+    void testOAuthCredentialAccess()
+    {
+        final ServiceBinding binding =
+            new ServiceBindingBuilder(ServiceIdentifier.DESTINATION)
+                .with("name", "asdf")
+                .with("credentials.name", "foo")
+                .with("credentials.uaa.name", "bar")
+                .build();
+        final ServiceBindingDestinationOptions options = ServiceBindingDestinationOptions.forService(binding).build();
+
+        sut = new DefaultOAuth2PropertySupplier(options);
+
+        assertThat(sut.getOAuthCredential(String.class, "name")).contains("bar");
+        assertThat(sut.getOAuthCredential(String.class, "uaa", "name")).isEmpty();
+
+        assertThatThrownBy(() -> sut.getOAuthCredentialOrThrow(String.class, "bar"))
+            .isInstanceOf(DestinationAccessException.class);
+    }
+
+    @Test
+    void testClientSecretIsTheDefault()
+    {
+        final ServiceBindingDestinationOptions options =
+            ServiceBindingDestinationOptions
+                .forService(new ServiceBindingBuilder(ServiceIdentifier.DESTINATION).build())
+                .build();
+
+        sut = new DefaultOAuth2PropertySupplier(options);
+
+        assertThat(sut.getCredentialType()).isEqualTo(CredentialType.BINDING_SECRET);
+    }
+
+    @Test
+    void testCredentialTypeInstanceSecret()
+    {
+        final ServiceBinding binding =
+            new ServiceBindingBuilder(ServiceIdentifier.of("testInstanceSecret"))
+                .with("credentials.uaa.credential-type", "instance-secret")
+                .build();
+        final ServiceBindingDestinationOptions options = ServiceBindingDestinationOptions.forService(binding).build();
+
+        sut = new DefaultOAuth2PropertySupplier(options);
+
+        assertThat(sut.getCredentialType()).isEqualTo(CredentialType.INSTANCE_SECRET);
+        assertThatCode(sut::getClientIdentity)
+            .isInstanceOf(DestinationAccessException.class)
+            .hasMessage("Failed to resolve property [uaa][clientid] from service binding.");
+    }
+
+    @Test
+    void testCredentialTypeX509()
+    {
+        final ServiceBinding binding =
+            new ServiceBindingBuilder(ServiceIdentifier.of("testX509"))
+                .with("credentials.uaa.credential-type", "x509")
+                .with("credentials.uaa.clientid", "id")
+                .with("credentials.uaa.certificate", "cert")
+                .with("credentials.uaa.key", "key")
+                .build();
+        final ServiceBindingDestinationOptions options = ServiceBindingDestinationOptions.forService(binding).build();
+
+        sut = new DefaultOAuth2PropertySupplier(options);
+
+        assertThat(sut.getCredentialType()).isEqualTo(CredentialType.X509);
+        assertThat(sut.getClientIdentity()).isInstanceOfSatisfying(ClientCertificate.class, cc -> {
+            assertThat(cc.getId()).isEqualTo("id");
+            assertThat(cc.getCertificate()).isEqualTo("cert");
+            assertThat(cc.getKey()).isEqualTo("key");
+        });
+    }
+
+    @RequiredArgsConstructor
+    private static final class ServiceBindingBuilder
+    {
+        @Nonnull
+        private final Map<String, Object> properties = new HashMap<>();
+
+        @Nonnull
+        private final ServiceIdentifier serviceIdentifier;
+
+        @Nonnull
+        @SuppressWarnings( "unchecked" )
+        ServiceBindingBuilder with( @Nonnull final String key, @Nonnull final Object value )
+        {
+            final String[] paths = key.split("\\.");
+
+            Map<String, Object> currentContainer = properties;
+            for( int pathIndex = 0; pathIndex < paths.length - 1; ++pathIndex ) {
+                final String path = paths[pathIndex];
+                final Object subContainer = currentContainer.computeIfAbsent(path, k -> new HashMap<String, Object>());
+
+                if( !(subContainer instanceof Map) ) {
+                    throw new IllegalStateException(String.format("Unable to set value '%s' = '%s'.", key, value));
+                }
+
+                currentContainer = (Map<String, Object>) subContainer;
+            }
+
+            currentContainer.put(paths[paths.length - 1], value);
+            return this;
+        }
+
+        @Nonnull
+        ServiceBinding build()
+        {
+            return DefaultServiceBinding
+                .builder()
+                .copy(properties)
+                .withCredentialsKey("credentials")
+                .withServiceIdentifier(serviceIdentifier)
+                .build();
+        }
+    }
+}
---
+++ cloudplatform/connectivity-ztis/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/OAuth2OnPremiseIntegrationTest.java
@@ -0,0 +1,79 @@
+package com.sap.cloud.sdk.cloudplatform.connectivity;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.ServiceBindingDestinationOptions.Options.ProxyOptions;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.ServiceBindingTestUtility.bindingWithCredentials;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+import java.util.Collection;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.Test;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.sap.cloud.environment.servicebinding.api.ServiceBinding;
+import com.sap.cloud.environment.servicebinding.api.ServiceIdentifier;
+
+@SuppressWarnings( "unchecked" )
+@WireMockTest
+class OAuth2OnPremiseIntegrationTest
+{
+    @Test
+    void destinationGetHeadersShouldRetrieveProxyAuthHeaderJustOnce( @Nonnull final WireMockRuntimeInfo wm )
+    {
+        stubFor(
+            post("/oauth/token")
+                .willReturn(
+                    okJson(
+                        "{\"access_token\": \"token\", \"token_type\": \"Bearer\", \"expires_in\": 50000, \"scope\": \"uaa.resource\", \"jti\": \"abc456\"}")));
+
+        final DefaultHttpDestination destToBeProxied = DefaultHttpDestination.builder("target.system").build();
+
+        final ServiceBindingDestinationOptions connectivityOptions =
+            createOptionsWithCredentials(
+                destToBeProxied,
+                entry("url", wm.getHttpBaseUrl()),
+                entry("clientid", "clientid"),
+                entry("clientsecret", "clientsecret"),
+                entry("onpremise_proxy_host", "some.host"),
+                entry("onpremise_proxy_http_port", 1234));
+
+        final OAuth2ServiceBindingDestinationLoader loader = new OAuth2ServiceBindingDestinationLoader();
+
+        final HttpDestination destination1 = loader.getDestination(connectivityOptions);
+        final HttpDestination destination2 = loader.getDestination(connectivityOptions);
+
+        // No caching of destinations
+        assertThat(destination1).isNotSameAs(destination2);
+
+        final Collection<Header> headers1 = destination1.getHeaders();
+        final Collection<Header> headers2 = destination2.getHeaders();
+
+        assertThat(headers1).containsExactlyInAnyOrderElementsOf(headers2);
+        assertThat(headers1).contains(new Header(HttpHeaders.PROXY_AUTHORIZATION, "Bearer token"));
+
+        verify(1, postRequestedFor(urlEqualTo("/oauth/token")));
+    }
+
+    private ServiceBindingDestinationOptions createOptionsWithCredentials(
+        final HttpDestination destToBeProxied,
+        final Map.Entry<String, Object>... entries )
+    {
+        final ServiceBinding binding = bindingWithCredentials(ServiceIdentifier.CONNECTIVITY, entries);
+
+        return ServiceBindingDestinationOptions
+            .forService(binding)
+            .withOption(ProxyOptions.destinationToBeProxied(destToBeProxied))
+            .build();
+    }
+}
