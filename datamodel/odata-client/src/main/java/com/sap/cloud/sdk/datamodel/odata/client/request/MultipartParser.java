229eacdbeef38e83d11431a8c520ca43780c4677
Alexander Dümont <alexander.duemont@sap.com>
2024-03-05 16:18:33 +0100

    Fix compiler warning, I guess

--- datamodel/odata-client/src/main/java/com/sap/cloud/sdk/datamodel/odata/client/request/MultipartParser.java
+++ datamodel/odata-client/src/main/java/com/sap/cloud/sdk/datamodel/odata/client/request/MultipartParser.java
@@ -5,6 +5,7 @@
 package com.sap.cloud.sdk.datamodel.odata.client.request;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
@@ -252,7 +253,7 @@ class MultipartParser implements AutoCloseable
 
     @Override
     public void close()
-        throws Exception
+        throws IOException
     {
         reader.close();
     }
