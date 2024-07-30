9d34902af479acac0e00662356455156d32d4b98
Alexander Dümont <alexander.duemont@sap.com>
2024-07-30 14:16:19 +0200

    Update test assertion

--- datamodel/openapi/openapi-generator/src/test/resources/DataModelGeneratorIntegrationTest/input-spec-with-anyof-oneof/output/com/sap/cloud/sdk/services/anyofoneof/api/DefaultApi.java
+++ datamodel/openapi/openapi-generator/src/test/resources/DataModelGeneratorIntegrationTest/input-spec-with-anyof-oneof/output/com/sap/cloud/sdk/services/anyofoneof/api/DefaultApi.java
@@ -71,7 +71,7 @@ public class DefaultApi extends AbstractOpenApiService {
      * @return RootObject
      * @throws OpenApiRequestException if an error occurs while attempting to invoke the API
      */
-    @Nonnull
+    @Nullable
     public RootObject someEndpointPost( @Nullable final RootObject rootObject) throws OpenApiRequestException {
         final Object localVarPostBody = rootObject;
         
