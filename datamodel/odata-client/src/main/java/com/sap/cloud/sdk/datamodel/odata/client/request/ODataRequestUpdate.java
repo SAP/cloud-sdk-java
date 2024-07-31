048ba6a82184a698f4a5cc73c473764fd22d24ce
Alexander Dümont <alexander.duemont@sap.com>
2024-07-31 13:34:04 +0200

    Format

--- datamodel/odata-client/src/main/java/com/sap/cloud/sdk/datamodel/odata/client/request/ODataRequestUpdate.java
+++ datamodel/odata-client/src/main/java/com/sap/cloud/sdk/datamodel/odata/client/request/ODataRequestUpdate.java
@@ -12,7 +12,6 @@ import javax.annotation.Nullable;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
 import org.apache.http.client.HttpClient;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
