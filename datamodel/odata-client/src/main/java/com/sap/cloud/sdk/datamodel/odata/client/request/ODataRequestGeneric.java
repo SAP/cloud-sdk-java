9e8ccaa098606bc17a779e85be1e9c207b5f68d5
Matthias Kuhr <matthias.kuhr@sap.com>
2024-08-28 10:49:46 +0200

    F

--- datamodel/odata-client/src/main/java/com/sap/cloud/sdk/datamodel/odata/client/request/ODataRequestGeneric.java
+++ datamodel/odata-client/src/main/java/com/sap/cloud/sdk/datamodel/odata/client/request/ODataRequestGeneric.java
@@ -7,7 +7,6 @@ package com.sap.cloud.sdk.datamodel.odata.client.request;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
