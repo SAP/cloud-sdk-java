f357f1e448964f392de51088e4b0adffdc331713
Alexander Dümont <alexander.duemont@sap.com>
2024-08-29 15:46:35 +0200

    Reduce annotation syntax

--- cloudplatform/cloudplatform-connectivity/src/main/java/com/sap/cloud/sdk/cloudplatform/connectivity/DefaultHttpDestination.java
+++ cloudplatform/cloudplatform-connectivity/src/main/java/com/sap/cloud/sdk/cloudplatform/connectivity/DefaultHttpDestination.java
@@ -558,20 +558,20 @@ public final class DefaultHttpDestination implements HttpDestination
 
         final DefaultDestination.Builder builder = DefaultDestination.builder();
 
-        @Setter( onParam = @__( { @Nullable } ), onMethod = @__( { @Nonnull } ), value = AccessLevel.PACKAGE )
+        @Setter( onParam_ = @Nullable, onMethod_ = @Nonnull, value = AccessLevel.PACKAGE )
         private DefaultHttpDestinationBuilderProxyHandler proxyHandler =
             new DefaultHttpDestinationBuilderProxyHandler();
 
         /**
          * The {@link KeyStore} to be used when communicating over HTTP.
          */
-        @Setter( onParam = @__( { @Nullable } ), onMethod = @__( { @Nonnull } ) )
+        @Setter( onParam_ = @Nullable, onMethod_ = @Nonnull )
         KeyStore keyStore = null;
 
         /**
          * The trust store to be used when communicating over HTTP.
          */
-        @Setter( onParam = @__( { @Nullable } ), onMethod = @__( { @Nonnull } ) )
+        @Setter( onParam_ = @Nullable, onMethod_ = @Nonnull )
         KeyStore trustStore = null;
 
         final List<DestinationHeaderProvider> customHeaderProviders = new ArrayList<>();
