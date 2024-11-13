0ff09bd268d9c2c31f9314a951f1f5a57dcbd3cb
Alexander Dümont <alexander_duemont@web.de>
2024-11-13 10:23:52 +0100

    Initial

--- cloudplatform/connectivity-apache-httpclient5/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/ApacheHttpClient5FactoryBuilderTest.java
+++ cloudplatform/connectivity-apache-httpclient5/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/ApacheHttpClient5FactoryBuilderTest.java
@@ -4,36 +4,9 @@
 
 package com.sap.cloud.sdk.cloudplatform.connectivity;
 
-import static java.util.function.Predicate.not;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.ok;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static com.sap.cloud.sdk.cloudplatform.connectivity.ApacheHttpClient5FactoryBuilder.TlsUpgrade.DISABLED;
-import static com.sap.cloud.sdk.cloudplatform.connectivity.ApacheHttpClient5FactoryBuilder.TlsUpgrade.ENABLED;
-import static com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination.builder;
-import static com.sap.cloud.sdk.cloudplatform.connectivity.ProxyType.INTERNET;
-import static com.sap.cloud.sdk.cloudplatform.connectivity.ProxyType.ON_PREMISE;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-import java.util.List;
-
-import javax.annotation.Nonnull;
-
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
 import org.junit.jupiter.api.Test;
-
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.http.Request;
-import com.github.tomakehurst.wiremock.matching.MatchResult;
-import com.github.tomakehurst.wiremock.matching.ValueMatcher;
-import com.github.tomakehurst.wiremock.stubbing.SubEvent;
-
-import lombok.SneakyThrows;
 
 class ApacheHttpClient5FactoryBuilderTest
 {
@@ -43,79 +16,5 @@ class ApacheHttpClient5FactoryBuilderTest
     {
         // make sure we can build a new factory instance without supplying any parameters
         assertThatNoException().isThrownBy(() -> new ApacheHttpClient5FactoryBuilder().build());
-    }
-
-    @Test
-    void testTlsUpgradeToggle()
-    {
-        var server = new WireMockServer(wireMockConfig().dynamicPort());
-        server.stubFor(get(anyUrl()).willReturn(ok()));
-        server.start();
-
-        var service = server.baseUrl();
-        var proxy = ProxyConfiguration.of(server.baseUrl());
-
-        var destInternet = builder(service).trustAllCertificates().build();
-        var destOnPremise = builder(service).proxyType(ON_PREMISE).proxyConfiguration(proxy).buildInternal();
-        var destProxy = builder(service).trustAllCertificates().proxyType(INTERNET).proxyConfiguration(proxy).build();
-        var destTlsVersion = builder(service).trustAllCertificates().tlsVersion("TLSv1.1").build();
-
-        ApacheHttpClient5Factory sut;
-
-        // force upgrade=true
-        sut = new ApacheHttpClient5FactoryBuilder().tlsUpgrade(ENABLED).build();
-        new TlsUpgradeAssertion(server, sut)
-            .withUpgradeHeader(destInternet)
-            .withUpgradeHeader(destProxy)
-            .withUpgradeHeader(destOnPremise)
-            .withUpgradeHeader(destTlsVersion);
-
-        // force upgrade=false
-        sut = new ApacheHttpClient5FactoryBuilder().tlsUpgrade(DISABLED).build();
-        new TlsUpgradeAssertion(server, sut)
-            .withoutUpgradeHeader(destInternet)
-            .withoutUpgradeHeader(destProxy)
-            .withoutUpgradeHeader(destOnPremise)
-            .withoutUpgradeHeader(destTlsVersion);
-
-        // default
-        sut = new ApacheHttpClient5FactoryBuilder().build();
-        new TlsUpgradeAssertion(server, sut)
-            .withUpgradeHeader(destInternet)
-            .withUpgradeHeader(destProxy)
-            .withoutUpgradeHeader(destOnPremise)
-            .withoutUpgradeHeader(destTlsVersion);
-    }
-
-    record TlsUpgradeAssertion(WireMockServer server, ApacheHttpClient5Factory factory) {
-        static ValueMatcher<Request> allowedHeaders(String... headers) {
-            return req -> {
-                var excess = req.getAllHeaderKeys().stream().filter(not(List.of(headers)::contains)).map(SubEvent::error).toList();
-                return excess.isEmpty() ? MatchResult.exactMatch() : MatchResult.noMatch(excess);
-            };
-        }
-
-        @SneakyThrows
-        private TlsUpgradeAssertion withUpgradeHeader(
-            @Nonnull final DefaultHttpDestination dest )
-        {
-            factory.createHttpClient(dest).execute(new HttpGet("/foo"), new BasicHttpClientResponseHandler());
-            server.verify(getRequestedFor(anyUrl())
-                .andMatching(allowedHeaders("Connection", "Host", "User-Agent", "Accept-Encoding", "Upgrade"))
-                .withHeader("Connection", equalTo("Upgrade"))
-                .withHeader("Upgrade", equalTo("TLS/1.2")));
-            return this;
-        }
-
-        @SneakyThrows
-        private TlsUpgradeAssertion withoutUpgradeHeader(
-            @Nonnull final DefaultHttpDestination dest )
-        {
-            factory.createHttpClient(dest).execute(new HttpGet("/foo"), new BasicHttpClientResponseHandler());
-            server.verify(getRequestedFor(anyUrl())
-                .andMatching(allowedHeaders("Connection", "Host", "User-Agent", "Accept-Encoding"))
-                .withHeader("Connection", equalTo("keep-alive")));
-            return this;
-        }
     }
 }
---
+++ cloudplatform/connectivity-apache-httpclient5/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/ApacheHttpClient5HeaderTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+
+package com.sap.cloud.sdk.cloudplatform.connectivity;
+
+import static java.util.function.Predicate.not;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.ApacheHttpClient5FactoryBuilder.TlsUpgrade;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.ApacheHttpClient5FactoryBuilder.TlsUpgrade.AUTOMATIC;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.ApacheHttpClient5FactoryBuilder.TlsUpgrade.DISABLED;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.ApacheHttpClient5FactoryBuilder.TlsUpgrade.ENABLED;
+import static com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination.builder;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
+
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.github.tomakehurst.wiremock.matching.MatchResult;
+import com.github.tomakehurst.wiremock.matching.ValueMatcher;
+import com.github.tomakehurst.wiremock.stubbing.SubEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.Value;
+
+import javax.annotation.Nonnull;
+
+@WireMockTest
+class ApacheHttpClient5HeaderTest
+{
+    @RequiredArgsConstructor
+    enum TestDestination
+    {
+        INTERNET(url -> builder(url).build()),
+        PROXY(url -> builder(url).proxyType(ProxyType.INTERNET).proxyConfiguration(ProxyConfiguration.of(url)).build()),
+        ON_PREMISE(
+            url -> builder(url)
+                .proxyType(ProxyType.ON_PREMISE)
+                .proxyConfiguration(ProxyConfiguration.of(url))
+                .buildInternal()),
+        TLS_VERSION(url -> builder(url).tlsVersion("TLSv1.1").build());
+
+        private final Function<String, HttpDestination> destinationBuilder;
+    }
+
+    @RequiredArgsConstructor
+    enum TestAssertion
+    {
+        DEFAULT(List.of("Connection", "Host", "User-Agent", "Accept-Encoding"), Map.of()),
+        UPGRADE(
+            List.of("Connection", "Host", "User-Agent", "Accept-Encoding", "Upgrade"),
+            Map.of("Connection", "Upgrade", "Upgrade", "TLS/1.2"));
+
+        private final List<String> headersAllowed;
+        private final Map<String, String> headersRequired;
+    }
+
+    @Value
+    static class TestCase
+    {
+        TestDestination destination;
+        TlsUpgrade tlsFlag;
+        TestAssertion assertion;
+    }
+
+    @SuppressWarnings( "unused" )
+    private static final TestCase[] TEST_CASES =
+        {
+            // Forced HTTP/TLS upgrade
+            new TestCase(TestDestination.INTERNET, ENABLED, TestAssertion.UPGRADE),
+            new TestCase(TestDestination.PROXY, ENABLED, TestAssertion.UPGRADE),
+            new TestCase(TestDestination.ON_PREMISE, ENABLED, TestAssertion.UPGRADE),
+            new TestCase(TestDestination.TLS_VERSION, ENABLED, TestAssertion.UPGRADE),
+
+            // Disabled HTTP/TLS upgrade
+            new TestCase(TestDestination.INTERNET, DISABLED, TestAssertion.DEFAULT),
+            new TestCase(TestDestination.PROXY, DISABLED, TestAssertion.DEFAULT),
+            new TestCase(TestDestination.ON_PREMISE, DISABLED, TestAssertion.DEFAULT),
+            new TestCase(TestDestination.TLS_VERSION, DISABLED, TestAssertion.DEFAULT),
+
+            // Automatic HTTP/TLS upgrade
+            new TestCase(TestDestination.INTERNET, AUTOMATIC, TestAssertion.UPGRADE),
+            new TestCase(TestDestination.PROXY, AUTOMATIC, TestAssertion.UPGRADE),
+            new TestCase(TestDestination.ON_PREMISE, AUTOMATIC, TestAssertion.DEFAULT),
+            new TestCase(TestDestination.TLS_VERSION, AUTOMATIC, TestAssertion.DEFAULT) };
+
+    @SneakyThrows
+    @ParameterizedTest
+    @FieldSource( "TEST_CASES" )
+    void testHeader( @Nonnull final TestCase testCase, @Nonnull final WireMockRuntimeInfo server )
+    {
+        stubFor(get(anyUrl()).willReturn(ok()));
+
+        var sut = new ApacheHttpClient5FactoryBuilder().tlsUpgrade(testCase.tlsFlag).build();
+        var dest = testCase.destination.destinationBuilder.apply(server.getHttpBaseUrl());
+        sut.createHttpClient(dest).execute(new HttpGet("/foo"), new BasicHttpClientResponseHandler());
+
+        var request = getRequestedFor(anyUrl()).andMatching(allowedHeaders(testCase.assertion.headersAllowed));
+        testCase.assertion.headersRequired.forEach(( k, v ) -> request.withHeader(k, equalTo(v)));
+        verify(request);
+    }
+
+    static ValueMatcher<Request> allowedHeaders( @Nonnull final List<String> headers )
+    {
+        return req -> {
+            var excess = req.getAllHeaderKeys().stream().filter(not(headers::contains)).map(SubEvent::error).toList();
+            return excess.isEmpty() ? MatchResult.exactMatch() : MatchResult.noMatch(excess);
+        };
+    }
+}
