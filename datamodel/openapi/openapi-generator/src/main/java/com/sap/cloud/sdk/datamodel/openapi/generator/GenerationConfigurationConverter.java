2c8c75506be1880cc695b3fa374552e6a77c10b3
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2025-02-27 16:33:21 +0100

    format

--- datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/GenerationConfigurationConverter.java
+++ datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/GenerationConfigurationConverter.java
@@ -40,7 +40,8 @@ import static com.sap.cloud.sdk.datamodel.openapi.generator.GeneratorCustomPrope
  * expects
  */
 @Slf4j
-class GenerationConfigurationConverter {
+class GenerationConfigurationConverter
+{
     private static final String IS_RELEASED_PROPERTY_KEY = "isReleased";
     private static final String JAVA_8_PROPERTY_KEY = "java8";
     private static final String DATE_LIBRARY_PROPERTY_KEY = "dateLibrary";
@@ -48,19 +49,20 @@ class GenerationConfigurationConverter {
     private static final String SOURCE_FOLDER_PROPERTY_KEY = "sourceFolder";
     private static final String OPEN_API_NULLABLE_PROPERTY_KEY = "openApiNullable";
     static final String COPYRIGHT_PROPERTY_KEY = "copyrightHeader";
-    
+
     static final String SAP_COPYRIGHT_HEADER =
         "Copyright (c) " + Year.now() + " SAP SE or an SAP affiliate company. All rights reserved.";
     static final String TEMPLATE_DIRECTORY = Paths.get("openapi-generator").resolve("mustache-templates").toString();
     static final String LIBRARY_NAME = JavaClientCodegen.RESTTEMPLATE;
-    
+
     @Nonnull
     static ClientOptInput convertGenerationConfiguration(
         @Nonnull final GenerationConfiguration generationConfiguration,
-        @Nonnull final Path inputSpec) {
+        @Nonnull final Path inputSpec )
+    {
         setGlobalSettings(generationConfiguration);
         final var inputSpecFile = inputSpec.toString();
-        
+
         final var config = createCodegenConfig(generationConfiguration);
         config.setOutputDir(generationConfiguration.getOutputDirectory());
         config.setLibrary(LIBRARY_NAME);
@@ -68,59 +70,65 @@ class GenerationConfigurationConverter {
         config.setModelPackage(generationConfiguration.getModelPackage());
         config.setTemplateDir(TEMPLATE_DIRECTORY);
         config.additionalProperties().putAll(getAdditionalProperties(generationConfiguration));
-        
+
         final var clientOptInput = new ClientOptInput();
         clientOptInput.config(config);
         clientOptInput.openAPI(parseOpenApiSpec(inputSpecFile));
         return clientOptInput;
     }
-    
-    private static JavaClientCodegen createCodegenConfig(@Nonnull final GenerationConfiguration config) {
+
+    private static JavaClientCodegen createCodegenConfig( @Nonnull final GenerationConfiguration config )
+    {
         final var primitives = Set.of("String", "Integer", "Long", "Double", "Float", "Byte");
         final var doubleIs = Pattern.compile("^isIs[A-Z]").asPredicate();
-        return new JavaClientCodegen() {
-            @SuppressWarnings({"rawtypes", "RedundantSuppression"})
+        return new JavaClientCodegen()
+        {
+            @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
             @Override
             protected void updatePropertyForArray(
                 @Nonnull final CodegenProperty property,
-                @Nonnull final CodegenProperty innerProperty) {
+                @Nonnull final CodegenProperty innerProperty )
+            {
                 super.updatePropertyForArray(property, innerProperty);
-                
-                if (USE_FLOAT_ARRAYS.isEnabled(config) && innerProperty.isNumber && property.isArray) {
+
+                if( USE_FLOAT_ARRAYS.isEnabled(config) && innerProperty.isNumber && property.isArray ) {
                     property.dataType = "float[]";
                     property.datatypeWithEnum = "float[]";
                     property.isArray = false; // set false to omit `add{{nameInPascalCase}}Item(...)` convenience method
                     property.vendorExtensions.put("isPrimitiveArray", true);
                 }
             }
-            
-            @SuppressWarnings({"rawtypes", "RedundantSuppression"})
+
+            @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
             @Override
             @Nullable
-            public String toDefaultValue(@Nonnull final CodegenProperty cp, @Nonnull final Schema schema) {
-                if (USE_FLOAT_ARRAYS.isEnabled(config) && "float[]".equals(cp.dataType)) {
+            public String toDefaultValue( @Nonnull final CodegenProperty cp, @Nonnull final Schema schema )
+            {
+                if( USE_FLOAT_ARRAYS.isEnabled(config) && "float[]".equals(cp.dataType) ) {
                     return null;
                 }
                 return super.toDefaultValue(cp, schema);
             }
-            
+
             @Override
             @Nullable
-            public String toBooleanGetter(@Nullable final String name) {
+            public String toBooleanGetter( @Nullable final String name )
+            {
                 final String result = super.toBooleanGetter(name);
-                if (FIX_REDUNDANT_IS_BOOLEAN_PREFIX.isEnabled(config) && result != null && doubleIs.test(result)) {
+                if( FIX_REDUNDANT_IS_BOOLEAN_PREFIX.isEnabled(config) && result != null && doubleIs.test(result) ) {
                     return "is" + result.substring(4);
                 }
                 return result;
             }
-            
+
             // Custom processor to inject "x-return-nullable" extension
             @Override
             @Nonnull
             public OperationsMap postProcessOperationsWithModels(
                 @Nonnull final OperationsMap ops,
-                @Nonnull final List<ModelMap> allModels) {
-                for (final CodegenOperation op : ops.getOperations().getOperation()) {
+                @Nonnull final List<ModelMap> allModels )
+            {
+                for( final CodegenOperation op : ops.getOperations().getOperation() ) {
                     final var noContent =
                         op.isResponseOptional
                             || op.responses == null
@@ -129,51 +137,53 @@ class GenerationConfigurationConverter {
                 }
                 return super.postProcessOperationsWithModels(ops, allModels);
             }
-            
-            @SuppressWarnings({"rawtypes", "RedundantSuppression"})
+
+            @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
             @Override
             protected void updateModelForComposedSchema(
                 @Nonnull final CodegenModel m,
                 @Nonnull final Schema schema,
-                @Nonnull final Map<String, Schema> allDefinitions) {
+                @Nonnull final Map<String, Schema> allDefinitions )
+            {
                 super.updateModelForComposedSchema(m, schema, allDefinitions);
-                
-                if (USE_ONE_OF_CREATORS.isEnabled(config)) {
+
+                if( USE_ONE_OF_CREATORS.isEnabled(config) ) {
                     useCreatorsForInterfaceSubtypes(m);
                 }
             }
-            
+
             /**
              * Use JsonCreator for interface sub-types in case there are any primitives.
              *
              * @param m
              *            The model to update.
              */
-            private void useCreatorsForInterfaceSubtypes(@Nonnull final CodegenModel m) {
-                if (m.discriminator != null) {
+            private void useCreatorsForInterfaceSubtypes( @Nonnull final CodegenModel m )
+            {
+                if( m.discriminator != null ) {
                     return;
                 }
                 boolean useCreators = false;
-                for (final Set<String> candidates : List.of(m.anyOf, m.oneOf)) {
+                for( final Set<String> candidates : List.of(m.anyOf, m.oneOf) ) {
                     int nonPrimitives = 0;
                     final var candidatesSingle = new HashSet<String>();
                     final var candidatesMultiple = new HashSet<String>();
-                    
-                    for (final String candidate : candidates) {
-                        if (candidate.startsWith("List<")) {
+
+                    for( final String candidate : candidates ) {
+                        if( candidate.startsWith("List<") ) {
                             final var c1 = candidate.substring(5, candidate.length() - 1);
                             candidatesMultiple.add(c1);
                             useCreators = true;
                         } else {
                             candidatesSingle.add(candidate);
                             useCreators |= primitives.contains(candidate);
-                            if (!primitives.contains(candidate)) {
+                            if( !primitives.contains(candidate) ) {
                                 nonPrimitives++;
                             }
                         }
                     }
-                    if (useCreators) {
-                        if (nonPrimitives > 1) {
+                    if( useCreators ) {
+                        if( nonPrimitives > 1 ) {
                             final var msg =
                                 "Generating interface with mixed multiple non-primitive and primitive sub-types: {}. Deserialization may not work.";
                             log.warn(msg, m.name);
@@ -185,10 +195,11 @@ class GenerationConfigurationConverter {
                     }
                 }
             }
-            
-            @SuppressWarnings({"rawtypes", "RedundantSuppression"})
+
+            @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
             @Override
-            protected void updateModelForObject(@Nonnull final CodegenModel m, @Nonnull final Schema schema) {
+            protected void updateModelForObject( @Nonnull final CodegenModel m, @Nonnull final Schema schema )
+            {
                 // Disable additional attributes to prevent model classes from extending "HashMap"
                 // SAP Cloud SDK offers custom field APIs to handle additional attributes already
                 schema.setAdditionalProperties(Boolean.FALSE);
@@ -196,15 +207,16 @@ class GenerationConfigurationConverter {
             }
         };
     }
-    
-    private static void setGlobalSettings(@Nonnull final GenerationConfiguration configuration) {
-        if (configuration.isGenerateApis()) {
+
+    private static void setGlobalSettings( @Nonnull final GenerationConfiguration configuration )
+    {
+        if( configuration.isGenerateApis() ) {
             GlobalSettings.setProperty(CodegenConstants.APIS, "");
         }
-        if (configuration.isGenerateModels()) {
+        if( configuration.isGenerateModels() ) {
             GlobalSettings.setProperty(CodegenConstants.MODELS, "");
         }
-        if (configuration.isDebugModels()) {
+        if( configuration.isDebugModels() ) {
             GlobalSettings.setProperty("debugModels", "true");
         }
         GlobalSettings.setProperty(CodegenConstants.MODEL_TESTS, Boolean.FALSE.toString());
@@ -214,31 +226,33 @@ class GenerationConfigurationConverter {
         GlobalSettings.clearProperty(CodegenConstants.SUPPORTING_FILES);
         GlobalSettings.setProperty(CodegenConstants.HIDE_GENERATION_TIMESTAMP, Boolean.TRUE.toString());
     }
-    
-    private static OpenAPI parseOpenApiSpec(@Nonnull final String inputSpecFile) {
+
+    private static OpenAPI parseOpenApiSpec( @Nonnull final String inputSpecFile )
+    {
         final List<AuthorizationValue> authorizationValues = List.of();
         final var options = new ParseOptions();
         options.setResolve(true);
         final var spec = new OpenAPIParser().readLocation(inputSpecFile, authorizationValues, options);
-        if (!spec.getMessages().isEmpty()) {
+        if( !spec.getMessages().isEmpty() ) {
             log.warn("Parsing the specification yielded the following messages: {}", spec.getMessages());
         }
         return spec.getOpenAPI();
     }
-    
-    private static Map<String, Object> getAdditionalProperties(@Nonnull final GenerationConfiguration config) {
+
+    private static Map<String, Object> getAdditionalProperties( @Nonnull final GenerationConfiguration config )
+    {
         log.info("Using {} as {}.", ApiMaturity.class.getSimpleName(), config.getApiMaturity());
-        
+
         final Map<String, Object> result = new HashMap<>();
         result.put(CodegenConstants.HIDE_GENERATION_TIMESTAMP, Boolean.TRUE.toString());
-        
-        switch (config.getApiMaturity()) {
+
+        switch( config.getApiMaturity() ) {
             case RELEASED -> result.put(IS_RELEASED_PROPERTY_KEY, true);
             case BETA -> result.remove(IS_RELEASED_PROPERTY_KEY);
         }
-        
+
         final var copyrightHeader = config.useSapCopyrightHeader() ? SAP_COPYRIGHT_HEADER : config.getCopyrightHeader();
-        if (!Strings.isNullOrEmpty(copyrightHeader)) {
+        if( !Strings.isNullOrEmpty(copyrightHeader) ) {
             result.put(COPYRIGHT_PROPERTY_KEY, copyrightHeader);
         }
         result.put(CodegenConstants.SERIALIZABLE_MODEL, "false");
@@ -249,10 +263,10 @@ class GenerationConfigurationConverter {
         // this is set to false, to prevent issues with the JsonNullable annotation
         // long term fix part of BLI CLOUDECOSYSTEM-9843
         result.put(OPEN_API_NULLABLE_PROPERTY_KEY, "false");
-        
+
         // this allows the customer to override the default Cloud SDK settings above
-        config.getAdditionalProperties().forEach((k, v) -> {
-            if (result.containsKey(k)) {
+        config.getAdditionalProperties().forEach(( k, v ) -> {
+            if( result.containsKey(k) ) {
                 final var msg =
                     "Replacing default value \"{}\" for additional property \"{}\" with \"{}\" from user provided configuration.";
                 log.info(msg, result.get(k), k, v);
