1ea6c6b8eb4046ed9d4eef58a7c4124f9dea3c42
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2025-02-28 15:06:48 +0100

    Add static feature classes

--- datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/CustomJavaClientCodegen.java
+++ datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/CustomJavaClientCodegen.java
@@ -31,8 +31,6 @@ class CustomJavaClientCodegen extends JavaClientCodegen
 {
 
     private final GenerationConfiguration config;
-    private static final Predicate<String> DOUBLE_IS_PATTERN = Pattern.compile("^isIs[A-Z]").asPredicate();
-    private static final Set<String> PRIMITIVES = Set.of("String", "Integer", "Long", "Double", "Float", "Byte");
 
     public CustomJavaClientCodegen( GenerationConfiguration config )
     {
@@ -46,11 +44,8 @@ class CustomJavaClientCodegen extends JavaClientCodegen
     {
         super.updatePropertyForArray(property, innerProperty);
 
-        if( USE_FLOAT_ARRAYS.isEnabled(config) && innerProperty.isNumber && property.isArray ) {
-            property.dataType = "float[]";
-            property.datatypeWithEnum = "float[]";
-            property.isArray = false; // set false to omit `add{{nameInPascalCase}}Item(...)` convenience method
-            property.vendorExtensions.put("isPrimitiveArray", true);
+        if( USE_FLOAT_ARRAYS.isEnabled(config) ) {
+            UseFloatArrayFeature.updatePropertyForArray(property, innerProperty);
         }
     }
 
@@ -59,21 +54,38 @@ class CustomJavaClientCodegen extends JavaClientCodegen
     @Nullable
     public String toDefaultValue( @Nonnull final CodegenProperty cp, @Nonnull final Schema schema )
     {
-        if( USE_FLOAT_ARRAYS.isEnabled(config) && "float[]".equals(cp.dataType) ) {
-            return null;
+        String superValue = super.toDefaultValue(cp, schema);
+
+        if( USE_FLOAT_ARRAYS.isEnabled(config) ) {
+            return UseFloatArrayFeature.toDefaultValue(cp, superValue);
         }
-        return super.toDefaultValue(cp, schema);
+        return superValue;
+
     }
 
     @Override
     @Nullable
     public String toBooleanGetter( @Nullable final String name )
     {
-        final String result = super.toBooleanGetter(name);
-        if( FIX_REDUNDANT_IS_BOOLEAN_PREFIX.isEnabled(config) && result != null && DOUBLE_IS_PATTERN.test(result) ) {
-            return "is" + result.substring(4);
+        final String superValue = super.toBooleanGetter(name);
+        if( FIX_REDUNDANT_IS_BOOLEAN_PREFIX.isEnabled(config) ) {
+            return RedundantBooleanGetterFeature.toBooleanGetter(superValue);
         }
-        return result;
+        return superValue;
+    }
+
+    @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
+    @Override
+    protected void updateModelForComposedSchema(
+        @Nonnull final CodegenModel m,
+        @Nonnull final Schema schema,
+        @Nonnull final Map<String, Schema> allDefinitions )
+    {
+        super.updateModelForComposedSchema(m, schema, allDefinitions);
+
+        if( USE_ONE_OF_CREATORS.isEnabled(config) ) {
+            UseOneOfCreatorsFeature.updateModelForComposedSchema(m);
+        }
     }
 
     // Custom processor to inject "x-return-nullable" extension
@@ -95,69 +107,96 @@ class CustomJavaClientCodegen extends JavaClientCodegen
 
     @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
     @Override
-    protected void updateModelForComposedSchema(
-        @Nonnull final CodegenModel m,
-        @Nonnull final Schema schema,
-        @Nonnull final Map<String, Schema> allDefinitions )
-    {
-        super.updateModelForComposedSchema(m, schema, allDefinitions);
-
-        if( USE_ONE_OF_CREATORS.isEnabled(config) ) {
-            useCreatorsForInterfaceSubtypes(m);
-        }
-    }
-
-    /**
-     * Use JsonCreator for interface sub-types in case there are any primitives.
-     *
-     * @param m
-     *            The model to update.
-     */
-    private void useCreatorsForInterfaceSubtypes( @Nonnull final CodegenModel m )
-    {
-        if( m.discriminator != null ) {
-            return;
-        }
-        boolean useCreators = false;
-        for( final Set<String> candidates : List.of(m.anyOf, m.oneOf) ) {
-            int nonPrimitives = 0;
-            final var candidatesSingle = new HashSet<String>();
-            final var candidatesMultiple = new HashSet<String>();
-
-            for( final String candidate : candidates ) {
-                if( candidate.startsWith("List<") ) {
-                    final var c1 = candidate.substring(5, candidate.length() - 1);
-                    candidatesMultiple.add(c1);
-                    useCreators = true;
-                } else {
-                    candidatesSingle.add(candidate);
-                    useCreators |= PRIMITIVES.contains(candidate);
-                    if( !PRIMITIVES.contains(candidate) ) {
-                        nonPrimitives++;
-                    }
-                }
-            }
-            if( useCreators ) {
-                if( nonPrimitives > 1 ) {
-                    final var msg =
-                        "Generating interface with mixed multiple non-primitive and primitive sub-types: {}. Deserialization may not work.";
-                    log.warn(msg, m.name);
-                }
-                candidates.clear();
-                final var monads = Map.of("single", candidatesSingle, "multiple", candidatesMultiple);
-                m.vendorExtensions.put("x-monads", monads);
-                m.vendorExtensions.put("x-is-one-of-interface", true); // enforce template usage
-            }
-        }
-    }
-
-    @SuppressWarnings( { "rawtypes", "RedundantSuppression" } )
-    @Override
     protected void updateModelForObject( @Nonnull final CodegenModel m, @Nonnull final Schema schema )
     {
         // Disable additional attributes to prevent model classes from extending "HashMap"
         // SAP Cloud SDK offers custom field APIs to handle additional attributes already
         schema.setAdditionalProperties(Boolean.FALSE);
         super.updateModelForObject(m, schema);
+    }
+
+    private static class UseFloatArrayFeature
+    {
+        static void updatePropertyForArray(
+            @Nonnull final CodegenProperty property,
+            @Nonnull final CodegenProperty innerProperty )
+        {
+            if( innerProperty.isNumber && property.isArray ) {
+                property.dataType = "float[]";
+                property.datatypeWithEnum = "float[]";
+                property.isArray = false; // set false to omit `add{{nameInPascalCase}}Item(...)` convenience method
+                property.vendorExtensions.put("isPrimitiveArray", true);
+            }
+        }
+
+        static String toDefaultValue( @Nonnull final CodegenProperty cp, String superValue )
+        {
+            if( "float[]".equals(cp.dataType) ) {
+                return null;
+            }
+            return superValue;
+        }
+    }
+
+    private static class UseOneOfCreatorsFeature
+    {
+        private static final Set<String> PRIMITIVES = Set.of("String", "Integer", "Long", "Double", "Float", "Byte");
+
+        /**
+         * Use JsonCreator for interface sub-types in case there are any primitives.
+         *
+         * @param m
+         *            The model to update.
+         */
+        static void updateModelForComposedSchema( @Nonnull final CodegenModel m )
+        {
+            if( m.discriminator != null ) {
+                return;
+            }
+            boolean useCreators = false;
+            for( final Set<String> candidates : List.of(m.anyOf, m.oneOf) ) {
+                int nonPrimitives = 0;
+                final var candidatesSingle = new HashSet<String>();
+                final var candidatesMultiple = new HashSet<String>();
+
+                for( final String candidate : candidates ) {
+                    if( candidate.startsWith("List<") ) {
+                        final var c1 = candidate.substring(5, candidate.length() - 1);
+                        candidatesMultiple.add(c1);
+                        useCreators = true;
+                    } else {
+                        candidatesSingle.add(candidate);
+                        useCreators |= PRIMITIVES.contains(candidate);
+                        if( !PRIMITIVES.contains(candidate) ) {
+                            nonPrimitives++;
+                        }
+                    }
+                }
+                if( useCreators ) {
+                    if( nonPrimitives > 1 ) {
+                        final var msg =
+                            "Generating interface with mixed multiple non-primitive and primitive sub-types: {}. Deserialization may not work.";
+                        log.warn(msg, m.name);
+                    }
+                    candidates.clear();
+                    final var monads = Map.of("single", candidatesSingle, "multiple", candidatesMultiple);
+                    m.vendorExtensions.put("x-monads", monads);
+                    m.vendorExtensions.put("x-is-one-of-interface", true); // enforce template usage
+                }
+            }
+        }
+    }
+
+    private static class RedundantBooleanGetterFeature
+    {
+        private static final Predicate<String> DOUBLE_IS_PATTERN = Pattern.compile("^isIs[A-Z]").asPredicate();
+
+        static String toBooleanGetter( String defaultValue )
+        {
+            if( defaultValue != null && DOUBLE_IS_PATTERN.test(defaultValue) ) {
+                return "is" + defaultValue.substring(4);
+            }
+            return defaultValue;
+        }
     }
 }
