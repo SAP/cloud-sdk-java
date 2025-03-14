bce32f21e9e5cc423789c637ce2b33730c239a1a
Alexander Dümont <alexander_duemont@web.de>
2025-03-14 14:50:25 +0100

    Format

--- datamodel/openapi/openapi-core/src/main/java/com/sap/cloud/sdk/services/openapi/apiclient/ApiClient.java
+++ datamodel/openapi/openapi-core/src/main/java/com/sap/cloud/sdk/services/openapi/apiclient/ApiClient.java
@@ -13,7 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.TimeZone;
 
 import javax.annotation.Nonnull;
