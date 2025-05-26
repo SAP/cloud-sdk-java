754c99366b598ef960accacab8c67f2f8e5144e9
Alexander Dümont <alexander.duemont@gmail.com>
2025-05-26 15:42:20 +0200

    initial

--- datamodel/odata-client/src/test/java/com/sap/cloud/sdk/datamodel/odata/client/request/ODataNextLinkTest.java
+++ datamodel/odata-client/src/test/java/com/sap/cloud/sdk/datamodel/odata/client/request/ODataNextLinkTest.java
@@ -1,9 +1,24 @@
 package com.sap.cloud.sdk.datamodel.odata.client.request;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import io.vavr.control.Try;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.entity.ContentType;
@@ -12,6 +27,10 @@ import org.apache.http.message.BasicHttpResponse;
 import org.junit.jupiter.api.Test;
 
 import com.sap.cloud.sdk.datamodel.odata.client.ODataProtocol;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
+
+import javax.annotation.Nonnull;
 
 class ODataNextLinkTest
 {
@@ -23,6 +42,102 @@ class ODataNextLinkTest
           }
         }
         """;
+
+    @RequiredArgsConstructor( staticName = "named" )
+    @AllArgsConstructor
+    @Accessors( fluent = true )
+    @Setter
+    static class QueryParameterCase
+    {
+        final String label;
+        Setup with;
+        Expectation expects;
+
+        @Builder
+        static class Setup
+        {
+            String destinationQuery;
+            String propertiesQuery;
+            String initialQuery;
+            String nextLinkQuery;
+        }
+
+        @Builder
+        static class Expectation
+        {
+            String initialQuerySent;
+            String nextLinkQueryParsed;
+            String nextLinkQuerySent;
+        }
+    }
+
+static QueryParameterCase[] QUERY_PARAMETERS_CASES =
+{
+// case 1: query-parameters from destination uri, destination properties, next-link and odata-request are distinct
+QueryParameterCase
+    .named("DISTINCT")
+    .with(
+        QueryParameterCase.Setup
+            .builder()
+            .destinationQuery("dest1=one&dest2=two")
+            .propertiesQuery("prop1=one&prop2=two")
+            .initialQuery("odata1=one&odata2=two")
+            .nextLinkQuery("next1=one&next2=two")
+            .build())
+    .expects(
+        QueryParameterCase.Expectation
+            .builder()
+            .initialQuerySent("dest1=one&dest2=two&odata1=one&odata2=two&prop1=one&prop2=two")
+            .nextLinkQueryParsed("$skiptoken=42&next1=one&next2=two")
+            .nextLinkQuerySent("dest1=one&dest2=two&$skiptoken=42&next1=one&next2=two&prop1=one&prop2=two")
+            .build()) };
+
+    @ParameterizedTest
+    @FieldSource( "QUERY_PARAMETERS_CASES" )
+    void testRemoveDuplicateQueryArguments1( @Nonnull final QueryParameterCase testCase )
+    {
+        final WireMockConfiguration wiremockConfig = wireMockConfig().dynamicPort();
+        final WireMockServer wiremock = new WireMockServer(wiremockConfig);
+        wiremock.start();
+
+        // TEST SETUP: Mock first request and response
+        final String initialRequest = "/v1/%s/endpoint?%s".formatted(testCase.label, testCase.expects.initialQuerySent);
+        final String initialResponse =
+            "{\"d\":{\"results\":[],\"__next\":\"/v1/%s/endpoint?$skiptoken=42&%s\"}}"
+                .formatted(testCase.label, testCase.with.nextLinkQuery);
+        wiremock.stubFor(get(urlEqualTo(initialRequest)).willReturn(okJson(initialResponse)));
+
+        // TEST SETUP: Mock second request and response
+        final String secondRequest = "/v1/%s/endpoint?%s".formatted(testCase.label, testCase.expects.nextLinkQuerySent);
+        final String secondResponse = "{\"d\":{\"results\":[]}}";
+        wiremock.stubFor(get(urlEqualTo(secondRequest)).willReturn(okJson(secondResponse)));
+
+        // TEST SETUP: construct destination and HttpClient
+        final String destinationUrl = wiremock.baseUrl() + "/?" + testCase.with.destinationQuery;
+        final DefaultHttpDestination.Builder destinationBuilder = DefaultHttpDestination.builder(destinationUrl);
+        for (final String queryArg : testCase.with.propertiesQuery.split("&")) {
+            final String[] queryArgParts = queryArg.split("=", 2);
+            destinationBuilder.property("URL.queries." + queryArgParts[0], queryArgParts[1]);
+        }
+        final HttpClient client = HttpClientAccessor.getHttpClient(destinationBuilder.build());
+
+        // TEST EXECUTION: Run OData request on behalf of HttpClient
+        final ODataRequestRead request =
+            new ODataRequestRead("/v1/" + testCase.label, "endpoint", testCase.with.initialQuery, ODataProtocol.V2);
+        final ODataRequestResultGeneric resultFirst = request.execute(client);
+
+        // TEST VALIDATION: Validate first actual request uri
+        assertThat(resultFirst.getNextLink())
+            .contains("/v1/%s/endpoint?%s".formatted(testCase.label, testCase.expects.nextLinkQueryParsed));
+        wiremock.verify(getRequestedFor(urlEqualTo(initialRequest)));
+
+        // TEST VALIDATION: Validate second actual request uri
+        final Try<ODataRequestResultGeneric> resultSecond = resultFirst.tryGetNextPage();
+        assertThat(resultSecond).isNotEmpty();
+        wiremock.verify(getRequestedFor(urlEqualTo(secondRequest)));
+
+        wiremock.shutdown();
+    }
 
     @Test
     void testNotParsedNextLinkV4()
