bd6e1f94dc902b6becb25ba313349fb2179fa30a
Matthias Kuhr <52661546+MatKuhr@users.noreply.github.com>
2025-08-18 09:32:15 +0200

    [Destinations] Cross Level Destination Support (#890)

--- cloudplatform/connectivity-destination-service/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/DestinationServiceAuthenticationTest.java
+++ cloudplatform/connectivity-destination-service/src/test/java/com/sap/cloud/sdk/cloudplatform/connectivity/DestinationServiceAuthenticationTest.java
@@ -40,7 +40,7 @@ class DestinationServiceAuthenticationTest
     private static final String BASIC_AUTH = "Zm9vOmJhcg==";
     private static final String DESTINATION_NAME = "CXT-HTTP-OAUTH";
 
-    private static final String SERVICE_PATH_DESTINATION = "/destinations/" + DESTINATION_NAME;
+    private static final String SERVICE_PATH_DESTINATION = "/v1/destinations/" + DESTINATION_NAME;
 
     @SuppressWarnings( "deprecation" )
     private static final DestinationOptions DESTINATION_RETRIEVAL_LOOKUP_EXCHANGE =
