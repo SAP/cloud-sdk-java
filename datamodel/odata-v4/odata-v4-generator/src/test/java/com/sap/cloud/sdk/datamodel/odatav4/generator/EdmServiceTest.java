c3201fb2a1f25663af3ce76ba842494080c0fa40
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2025-12-09 10:25:56 +0100

    formatting

--- datamodel/odata-v4/odata-v4-generator/src/test/java/com/sap/cloud/sdk/datamodel/odatav4/generator/EdmServiceTest.java
+++ datamodel/odata-v4/odata-v4-generator/src/test/java/com/sap/cloud/sdk/datamodel/odatav4/generator/EdmServiceTest.java
@@ -1,40 +1,49 @@
 package com.sap.cloud.sdk.datamodel.odatav4.generator;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.sap.cloud.sdk.datamodel.odata.utility.ServiceNameMappings;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.olingo.commons.api.edm.Edm;
 import org.apache.olingo.commons.api.edm.EdmEntityContainer;
 import org.apache.olingo.commons.api.edm.EdmSchema;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Optional;
+import com.google.common.collect.ArrayListMultimap;
+import com.sap.cloud.sdk.datamodel.odata.utility.ServiceNameMappings;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-
-class EdmServiceTest {
+class EdmServiceTest
+{
     private static final String SERVICE_NAME = "API_MATERIAL_DOCUMENT_SRV";
 
     @Test
-    void testServiceNameMappingsGenerated() {
+    void testServiceNameMappingsGenerated()
+    {
 
         // Mock ServiceNameMappings to return empty Optional (no stored mappings)
         final ServiceNameMappings mockMappings = mock(ServiceNameMappings.class);
         doReturn(Optional.empty()).when(mockMappings).getString(SERVICE_NAME + ".packageName");
         doReturn(Optional.empty()).when(mockMappings).getString(SERVICE_NAME + ".className");
 
-        final EdmService service = new EdmService(
-                SERVICE_NAME, mockMappings, createMockEdm(), mock(ServiceDetails.class),
-                ArrayListMultimap.create(), false);
+        final EdmService service =
+            new EdmService(
+                SERVICE_NAME,
+                mockMappings,
+                createMockEdm(),
+                mock(ServiceDetails.class),
+                ArrayListMultimap.create(),
+                false);
 
         assertThat(service.getJavaPackageName()).isEqualTo("apimaterialdocumentsrv");
         assertThat(service.getJavaClassName()).isEqualTo("APIMATERIALDOCUMENTSRV");
     }
 
     @Test
-    void testStoredServiceNameMappingsAreUnchanged() {
+    void testStoredServiceNameMappingsAreUnchanged()
+    {
         final String expectedPackageName = "apimaterialdocumentsrv";
         final String expectedClassName = "APIMATERIALDOCUMENTSRV";
 
@@ -43,15 +52,21 @@ class EdmServiceTest {
         doReturn(Optional.of(expectedPackageName)).when(mockMappings).getString(SERVICE_NAME + ".packageName");
         doReturn(Optional.of(expectedClassName)).when(mockMappings).getString(SERVICE_NAME + ".className");
 
-        final EdmService service = new EdmService(
-                SERVICE_NAME, mockMappings, createMockEdm(), mock(ServiceDetails.class),
-                ArrayListMultimap.create(), false);
+        final EdmService service =
+            new EdmService(
+                SERVICE_NAME,
+                mockMappings,
+                createMockEdm(),
+                mock(ServiceDetails.class),
+                ArrayListMultimap.create(),
+                false);
 
         assertThat(service.getJavaPackageName()).isEqualTo(expectedPackageName);
         assertThat(service.getJavaClassName()).isEqualTo(expectedClassName);
     }
 
-    private static Edm createMockEdm() {
+    private static Edm createMockEdm()
+    {
         final Edm mockEdm = mock(Edm.class);
         final EdmEntityContainer mockContainer = mock(EdmEntityContainer.class);
         final EdmSchema mockSchema = mock(EdmSchema.class);
