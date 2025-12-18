42ae27a07d471ce3cc48d59c22ab24b74e2b38fb
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2025-12-18 14:24:54 +0100

    Remove invokeAPI methods in BaseApi and *Api.java - *Api.invoke method bugged and unused

--- datamodel/openapi/openapi-api-apache-sample/src/main/java/com/sap/cloud/sdk/datamodel/openapi/apache/sample/api/OrchestrationConfigsApi.java
+++ datamodel/openapi/openapi-api-apache-sample/src/main/java/com/sap/cloud/sdk/datamodel/openapi/apache/sample/api/OrchestrationConfigsApi.java
@@ -800,47 +800,4 @@ public class OrchestrationConfigsApi extends BaseApi
                 localVarContentType,
                 localVarReturnType);
     }
-
-    @Override
-    public <T> T invokeAPI(
-        String url,
-        String method,
-        Object request,
-        TypeReference<T> returnType,
-        Map<String, String> additionalHeaders )
-        throws ApiException
-    {
-        String localVarPath = url.replace(apiClient.getBaseURL(), "");
-        StringJoiner localVarQueryStringJoiner = new StringJoiner("&");
-        List<Pair> localVarQueryParams = new ArrayList<Pair>();
-        List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
-        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
-        Map<String, String> localVarCookieParams = new HashMap<String, String>();
-        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
-
-        localVarHeaderParams.putAll(additionalHeaders);
-
-        final String[] localVarAccepts = { "application/json" };
-        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
-
-        final String[] localVarContentTypes = {
-
-        };
-        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
-
-        return apiClient
-            .invokeAPI(
-                localVarPath,
-                method,
-                localVarQueryParams,
-                localVarCollectionQueryParams,
-                localVarQueryStringJoiner.toString(),
-                request,
-                localVarHeaderParams,
-                localVarCookieParams,
-                localVarFormParams,
-                localVarAccept,
-                localVarContentType,
-                returnType);
-    }
 }
