3c7ee802cf95facb836b9abb85cbdfbe0b40eda1
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2026-01-16 15:47:54 +0100

    Add missing javadoc

--- datamodel/openapi/openapi-core/src/main/java/com/sap/cloud/sdk/services/openapi/apache/ResponseMetadataListener.java
+++ datamodel/openapi/openapi-core/src/main/java/com/sap/cloud/sdk/services/openapi/apache/ResponseMetadataListener.java
@@ -1,7 +1,18 @@
 package com.sap.cloud.sdk.services.openapi.apache;
 
+/**
+ * Listener for receiving metadata about HTTP responses.
+ *
+ * @since 5.25.0
+ */
 @FunctionalInterface
 public interface ResponseMetadataListener
 {
+    /**
+     * Called when an HTTP response is received.
+     *
+     * @param response
+     *            The response metadata.
+     */
     void onResponse( OpenApiResponse response );
 }
