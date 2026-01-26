60eef2060be743cd822269f0850bbe04f1238d7b
Roshin Rajan Panackal <roshin.rajan.panackal@sap.com>
2026-01-26 15:47:24 +0100

    Move all apache related code out

--- datamodel/openapi/openapi-core-apache/src/test/java/com/sap/cloud/sdk/services/openapi/apache/genericparameter/GenericParameterTest.java
+++ datamodel/openapi/openapi-core-apache/src/test/java/com/sap/cloud/sdk/services/openapi/apache/genericparameter/GenericParameterTest.java
@@ -1,4 +1,4 @@
-package com.sap.cloud.sdk.services.openapi.genericparameter;
+package com.sap.cloud.sdk.services.openapi.apache.genericparameter;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
@@ -7,24 +7,22 @@ import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.client.WireMock;
@@ -32,8 +30,10 @@ import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination;
 import com.sap.cloud.sdk.cloudplatform.connectivity.Destination;
-import com.sap.cloud.sdk.services.openapi.core.AbstractOpenApiService;
-import com.sap.cloud.sdk.services.openapi.core.OpenApiRequestException;
+import com.sap.cloud.sdk.services.openapi.apache.apiclient.ApiClient;
+import com.sap.cloud.sdk.services.openapi.apache.apiclient.BaseApi;
+import com.sap.cloud.sdk.services.openapi.apache.apiclient.Pair;
+import com.sap.cloud.sdk.services.openapi.apache.core.OpenApiRequestException;
 
 import lombok.Builder;
 import lombok.Value;
@@ -77,7 +77,7 @@ class GenericParameterTest
                 .fieldPopulatedAsJacksonJsonNode(jacksonJsonNode)
                 .build();
 
-        new TestSpringApi(httpDestination).testMethod(testModelPOST);
+        new TestApacheApi(httpDestination).testMethod(testModelPOST);
         verify(1, postRequestedFor(urlEqualTo("/api")).withRequestBody(equalTo(EXPECTED_BODY)));
     }
 
@@ -99,53 +99,59 @@ class GenericParameterTest
         Object fieldPopulatedAsJacksonJsonNode;
     }
 
-    private static class TestSpringApi extends AbstractOpenApiService
+    private static class TestApacheApi extends BaseApi
     {
-        public TestSpringApi( @Nonnull final Destination httpDestination )
+        public TestApacheApi( @Nonnull final Destination destination )
         {
-            super(httpDestination);
+            super(destination);
         }
 
         @Nullable
         public TestModel testMethod( @Nonnull final TestModelPOST body )
             throws OpenApiRequestException
         {
-            final Object postBody = body;
+            final Object localVarPostBody = body;
 
-            // verify the required parameter 'body' is set
+            // verify the required parameter 'pet' is set
             if( body == null ) {
-                throw new OpenApiRequestException("Missing the required parameter 'body' when calling createInstances");
+                throw new OpenApiRequestException("Missing the required parameter 'pet' when calling addPet")
+                    .statusCode(400);
             }
 
-            final String path = UriComponentsBuilder.fromPath("/api").build().toUriString();
+            // create path and map variables
+            final String localVarPath = "/api";
 
-            final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
-            final HttpHeaders headerParams = new HttpHeaders();
-            final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+            final StringJoiner localVarQueryStringJoiner = new StringJoiner("&");
+            String localVarQueryParameterBaseName;
+            final List<Pair> localVarQueryParams = new ArrayList<Pair>();
+            final List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
+            final Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+            final Map<String, Object> localVarFormParams = new HashMap<String, Object>();
 
-            final String[] accepts = { "application/json" };
-            final List<MediaType> accept = apiClient.selectHeaderAccept(accepts);
-            final String[] contentTypes = { "application/json" };
-            final MediaType contentType = apiClient.selectHeaderContentType(contentTypes);
+            final String[] localVarAccepts = { "application/json" };
+            final String localVarAccept = ApiClient.selectHeaderAccept(localVarAccepts);
 
-            final String[] authNames = new String[] { "Oauth2_AuthorizationCode", "Oauth2_ClientCredentials" };
+            final String[] localVarContentTypes = { "application/json" };
+            final String localVarContentType = ApiClient.selectHeaderContentType(localVarContentTypes);
 
-            final ParameterizedTypeReference<TestModel> returnType = new ParameterizedTypeReference<TestModel>()
+            final TypeReference<TestModel> localVarReturnType = new TypeReference<TestModel>()
             {
             };
             return apiClient
                 .invokeAPI(
-                    path,
-                    HttpMethod.POST,
-                    queryParams,
-                    postBody,
-                    headerParams,
-                    formParams,
-                    accept,
-                    contentType,
-                    authNames,
-                    returnType);
+                    localVarPath,
+                    "POST",
+                    localVarQueryParams,
+                    localVarCollectionQueryParams,
+                    localVarQueryStringJoiner.toString(),
+                    localVarPostBody,
+                    localVarHeaderParams,
+                    localVarFormParams,
+                    localVarAccept,
+                    localVarContentType,
+                    localVarReturnType);
         }
+
     }
 
     private static class TestModel
