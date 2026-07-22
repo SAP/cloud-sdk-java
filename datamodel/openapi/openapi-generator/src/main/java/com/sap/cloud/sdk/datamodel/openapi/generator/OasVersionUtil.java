fe0363194a6244998e6c6d9588f33ed526854e9a
Zhongpin Wang <zhongpin.wang@sap.com>
2026-07-22 17:01:38 +0200

    Remove unused overload in oas version util

--- datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/OasVersionUtil.java
+++ datamodel/openapi/openapi-generator/src/main/java/com/sap/cloud/sdk/datamodel/openapi/generator/OasVersionUtil.java
@@ -1,9 +1,6 @@
 package com.sap.cloud.sdk.datamodel.openapi.generator;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 import io.swagger.v3.oas.models.OpenAPI;
 
@@ -13,18 +10,9 @@ final class OasVersionUtil
     {
     }
 
-    static boolean isOas31( @Nullable final String version )
-    {
-        return version != null && version.startsWith("3.1");
-    }
-
     static boolean isOas31( @Nonnull final OpenAPI openAPI )
     {
-        return isOas31(openAPI.getOpenapi());
-    }
-
-    static boolean isOas31( @Nonnull final JsonNode rootNode )
-    {
-        return isOas31(rootNode.path("openapi").asText(null));
+        final String version = openAPI.getOpenapi();
+        return version != null && version.startsWith("3.1");
     }
 }
