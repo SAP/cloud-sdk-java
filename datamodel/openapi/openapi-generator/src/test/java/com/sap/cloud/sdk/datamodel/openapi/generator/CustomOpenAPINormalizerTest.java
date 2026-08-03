a0d7f6bac8a7a307d76a5af7d6006ee8ee15a169
Zhongpin Wang <zhongpin.wang@sap.com>
2026-08-03 16:58:37 +0200

    Write test for nullable warning capture

--- datamodel/openapi/openapi-generator/src/test/java/com/sap/cloud/sdk/datamodel/openapi/generator/CustomOpenAPINormalizerTest.java
+++ datamodel/openapi/openapi-generator/src/test/java/com/sap/cloud/sdk/datamodel/openapi/generator/CustomOpenAPINormalizerTest.java
@@ -2,10 +2,17 @@ package com.sap.cloud.sdk.datamodel.openapi.generator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.openapitools.codegen.OpenAPINormalizer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.Schema;
@@ -121,5 +128,56 @@ class CustomOpenAPINormalizerTest
         normalizer(oas30()).normalizeSchema(schema, new HashSet<>());
 
         assertThat(schema.getFormat()).isNull();
+    }
+
+    // --- nullable: true warning on $ref schema ---
+
+    @Test
+    void oas31RefSchemaWithNullableEmitsWarning()
+        throws Throwable
+    {
+        final Schema<?> schema = new Schema<>();
+        schema.set$ref("#/components/schemas/Foo");
+        schema.setNullable(true);
+
+        final String output = captureWarn(() -> normalizer(oas31()).normalizeReferenceSchema(schema));
+
+        assertThat(output).contains("nullable: true").contains("#/components/schemas/Foo");
+    }
+
+    @Test
+    void oas30RefSchemaWithNullableDoesNotWarn()
+        throws Throwable
+    {
+        final Schema<?> schema = new Schema<>();
+        schema.set$ref("#/components/schemas/Foo");
+        schema.setNullable(true);
+
+        final String output = captureWarn(() -> normalizer(oas30()).normalizeReferenceSchema(schema));
+
+        assertThat(output).doesNotContain("nullable: true");
+    }
+
+    private static String captureWarn( final Executable action )
+        throws Throwable
+    {
+        // slf4j-simple writes to System.err; temporarily lower its level to WARN so the message is emitted.
+        final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger(OpenAPINormalizer.class);
+        final Field levelField = slf4jLogger.getClass().getDeclaredField("currentLogLevel");
+        levelField.setAccessible(true);
+        final int original = levelField.getInt(slf4jLogger);
+        levelField.setInt(slf4jLogger, LocationAwareLogger.WARN_INT);
+
+        final PrintStream originalErr = System.err;
+        final ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(buf));
+        try {
+            action.execute();
+        }
+        finally {
+            System.setErr(originalErr);
+            levelField.setInt(slf4jLogger, original);
+        }
+        return buf.toString();
     }
 }
